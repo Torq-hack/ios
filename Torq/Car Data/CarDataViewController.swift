4cb4ea2dcc8117c088c7b4260d72320497d474aa
aKANJ <me@akanj.com>
2018-10-20 21:13:04 -0300

    Initial structure

---
+++ Torq/Car Data/CarDataViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CarDataViewController.swift
+//  Torq
+//
+//  Created by Jean Paul Marinho on 20/10/18.
+//  Copyright © 2018 aKANJ. All rights reserved.
+//
+
+import UIKit
+
+class CarDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
